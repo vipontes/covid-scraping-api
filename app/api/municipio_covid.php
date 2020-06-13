d9b1c430de9d610f585fc74531db43ff7527f485
Jeferson <jeferson@easify.net.br>
2020-06-13 08:57:05 -0300

    Adicionou endpoint para extracao de cidades críticas e controladas

--- app/api/municipio_covid.php
+++ app/api/municipio_covid.php
@@ -61,3 +61,117 @@ $app->get("/brasil/cidades", function ($request, $response) {
         return $this->response->withJson($result, $status);
     }
 })->add($middleware);
+
+/**
+ *
+ */
+$app->get("/brasil/cidades/criticas", function ($request, $response) {
+    require_once ('db/dbconnect.php');
+
+    try {
+        $query = "SELECT MAX(data_medicao) AS ultima_medicao FROM municipio_covid";
+        $sth = $db->prepare($query);
+        $sth->execute();
+        $maxDate = $sth->fetch();
+
+        $query = "SELECT
+        mc.municipio_covid_id,
+        mc.municipio_id,
+        m.nome,
+        m.populacao,
+        e.estado_id,
+        e.estado_sigla,
+        r.regiao_descricao,
+        mc.data_medicao,
+        mc.obitos,
+        mc.novos_casos,
+        mc.casos_acumulado,
+        Mortalidade(mc.obitos, m.populacao) AS mortalidade,
+        Letalidade(mc.obitos, mc.casos_acumulado) AS letalidade
+        FROM municipio_covid mc
+        INNER JOIN municipio m ON m.municipio_id = mc.municipio_id
+        INNER JOIN estado e ON m.estado_id = e.estado_id
+        INNER JOIN regiao r ON r.regiao_id = e.regiao_id
+        WHERE m.populacao >= 100000 AND mc.data_medicao = :day
+        ORDER BY obitos DESC, populacao DESC LIMIT 10";
+        $sth = $db->prepare($query);
+        $sth->execute(array(':day' => $maxDate['ultima_medicao']));
+        $data = $sth->fetchAll();
+        //
+        $status = 200;
+        $result = $data;
+        header('Content-Type: application/json');
+        return $this->response->withJson($result, $status);
+    } catch (PDOException $e) {
+        $status = 409;
+        $result = array();
+        $result["success"] = false;
+        $result["message"] = $e->getMessage();
+        header('Content-Type: application/json');
+        return $this->response->withJson($result, $status);
+    } catch (Exception $x) {
+        $status = 409;
+        $result = array();
+        $result["success"] = false;
+        $result["message"] = $x->getMessage();
+        header('Content-Type: application/json');
+        return $this->response->withJson($result, $status);
+    }
+})->add($middleware);
+
+/**
+ *
+ */
+$app->get("/brasil/cidades/controladas", function ($request, $response) {
+    require_once ('db/dbconnect.php');
+
+    try {
+        $query = "SELECT MAX(data_medicao) AS ultima_medicao FROM municipio_covid";
+        $sth = $db->prepare($query);
+        $sth->execute();
+        $maxDate = $sth->fetch();
+
+        $query = "SELECT
+        mc.municipio_covid_id,
+        mc.municipio_id,
+        m.nome,
+        m.populacao,
+        e.estado_id,
+        e.estado_sigla,
+        r.regiao_descricao,
+        mc.data_medicao,
+        mc.obitos,
+        mc.novos_casos,
+        mc.casos_acumulado,
+        Mortalidade(mc.obitos, m.populacao) AS mortalidade,
+        Letalidade(mc.obitos, mc.casos_acumulado) AS letalidade
+        FROM municipio_covid mc
+        INNER JOIN municipio m ON m.municipio_id = mc.municipio_id
+        INNER JOIN estado e ON m.estado_id = e.estado_id
+        INNER JOIN regiao r ON r.regiao_id = e.regiao_id
+        WHERE m.populacao >= 100000 AND mc.data_medicao = :day
+        ORDER BY obitos ASC, populacao DESC LIMIT 10";
+        $sth = $db->prepare($query);
+        $sth->execute(array(':day' => $maxDate['ultima_medicao']));
+        $data = $sth->fetchAll();
+        //
+        $status = 200;
+        $result = $data;
+        header('Content-Type: application/json');
+        return $this->response->withJson($result, $status);
+    } catch (PDOException $e) {
+        $status = 409;
+        $result = array();
+        $result["success"] = false;
+        $result["message"] = $e->getMessage();
+        header('Content-Type: application/json');
+        return $this->response->withJson($result, $status);
+    } catch (Exception $x) {
+        $status = 409;
+        $result = array();
+        $result["success"] = false;
+        $result["message"] = $x->getMessage();
+        header('Content-Type: application/json');
+        return $this->response->withJson($result, $status);
+    }
+})->add($middleware);
