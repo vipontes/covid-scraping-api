4a74ccb7c135649e02b0805401f3b551b1b4f1be
Jeferson <jeferson@easify.net.br>
2020-06-19 16:22:11 -0300

    Adicionou endpoint

--- app/api/municipio_covid.php
+++ app/api/municipio_covid.php
@@ -271,6 +271,61 @@ $app->get("/brasil/obitos/diario", function ($request, $response) {
 /**
  *
  */
+$app->get("/brasil/cidades/historico/{:municipioId}", function ($request, $response) {
+    require_once ('db/dbconnect.php');
+
+    $municipioId = $request->getAttribute('municipioId');
+
+    try {
+        $query = "SELECT
+        mc.municipio_covid_id,
+        mc.municipio_id,
+        m.nome,
+        m.populacao,
+        e.estado_id,
+        e.estado_sigla,
+        r.regiao_descricao,
+        mc.data_medicao,
+        mc.obitos,
+        mc.novos_casos,
+        mc.casos_acumulado,
+        mc.novos_obitos,
+        Mortalidade(mc.obitos, m.populacao) AS mortalidade,
+        Letalidade(mc.obitos, mc.casos_acumulado) AS letalidade
+        FROM municipio_covid mc
+        INNER JOIN municipio m ON m.municipio_id = mc.municipio_id
+        INNER JOIN estado e ON m.estado_id = e.estado_id
+        INNER JOIN regiao r ON r.regiao_id = e.regiao_id
+        WHERE mc.municipio_id = :municipioId
+        ORDER BY municipio_covid_id DESC LIMIT 45";
+        $sth = $db->prepare($query);
+        $sth->execute(array(':municipioId' => $municipioId));
+        $data = $sth->fetchAll();
+        //
+        $status = 200;
+        $result = array_reverse($data);
+        header('Content-Type: application/json');
+        return $this->response->withJson($result, $status);
+    } catch (PDOException $e) {
+        $status = 409;
+        $result = array();
+        $result["success"] = false;
+        $result["message"] = $e->getMessage();
+        header('Content-Type: application/json');
+        return $this->response->withJson($result, $status);
+    } catch (Exception $x) {
+        $status = 409;
+        $result = array();
+        $result["success"] = false;
+        $result["message"] = $x->getMessage();
+        header('Content-Type: application/json');
+        return $this->response->withJson($result, $status);
+    }
+})->add($middleware);
+
+/**
+ *
+ */
 $app->get("/brasil/estados", function ($request, $response) {
     require_once ('db/dbconnect.php');
 
